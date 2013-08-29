4a996883bd217b3c0ac6987f5bf82041ac1b17cc
Jan Voráček <jan@voracek.net>
2013-08-29 16:13:23 +0200

    Tests moved to test directory

---
+++ test/fastweb.dart
@@ -0,0 +1,11 @@
+library fastweb_tests;
+import 'package:unittest/unittest.dart';
+
+import 'package:fastweb/fastweb.dart';
+
+
+part 'basic_behaviour.dart';
+
+main() {
+  basic_behaviour_tests();
+}
