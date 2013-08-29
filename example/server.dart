7bda9c0ac41c70accef3053f8a15c935bcc87ad0
Jan Voráček <jan@voracek.net>
2013-08-29 16:18:04 +0200

    Updated sample

--- example/server.dart
+++ example/server.dart
@@ -1,7 +1,28 @@
 import 'dart:io';
+import 'dart:utf' as Utf;
 import 'package:fastweb/fastweb.dart';
+
+
+class RequestLog extends Middleware {
+  run(Request request, Response response, next()) {
+    print('${request.method}: ${request.uri.toString()}');
+    next();
+  }
+}
 
 main() {
   var fw = new Fastweb();
+  fw.use(new RequestLog());
+  fw.use(fw.router);
+
+  fw.get('/').listen((Request request, Response response){
+    response.write('Hello fastweb');
+    response.close();
+  });
+
+  fw.post('/').listen((request, response) {
+    response.close();
+  });
+
   HttpServer.bind('127.0.0.1', 8080).then(fw);
 }
